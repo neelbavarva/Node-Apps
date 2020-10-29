e69eea5806d46fa692c25a428a7dd3012722b53f
Neel Bavarva <neelbavarva5@gmail.com>
2020-10-29 10:31:18 +0530

    blog adder done

--- Blog-Adder/server.js
+++ Blog-Adder/server.js
@@ -1,33 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {
-    useNewUrlParser: true , 
-    useUnifiedTopology: true
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
 
-app.use(express.urlencoded({extended: false}))
-
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
-
-app.get('/', (req,res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Text description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Text description 2'
-    }
-]
-res.render('articles/index', { articles : articles })
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
 })
 
 app.use('/articles', articleRouter)
 
-app.listen(5000)
+app.listen(3000)
